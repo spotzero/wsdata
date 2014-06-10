a7e9209258e164a3bfcf9bbbb3c66d821a76d02b
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2014-06-10 16:36:56 -0400

    Added docs for the language plugins

--- wsdata.api.php
+++ wsdata.api.php
@@ -35,10 +35,76 @@
  */
 function hook_wsdata_language_plugin() {
   return array(
-    'header',
-    'argument',
-    'path',
-    'uri',
-    'default',
+    /**
+     * Header language plugin
+     *
+     * The header plugin adds a header value to the request
+     * which sets the language of the request.
+     *
+     * The value is the language key of the enabled languages on your site.
+     * You may override this in the settings array.
+     */
+    'header' => array(
+      'settings' => array(
+        'name' => 'Accept-Language',
+        'en' => 'en',
+        'fr' => 'fr',
+      ),
+      'form' => 'wsdata_language_plugin_header_form',
+      'file' => 'wsdata.admin.inc',
+    ),
+    /**
+     * Argument language plugin
+     *
+     * The argument plugin adds a query string argument (ex: ?lang=en).
+     */
+    'argument' => array(
+      'settings' => array(
+        'name' => 'lang',
+        'en' => 'en',
+        'fr' => 'fr',
+      ),
+      'form' => 'wsdata_language_plugin_argument_form',
+      'file' => 'wsdata.admin.inc',
+    ),
+    /**
+     * Path language plugin
+     *
+     * The path plugin puts the language into the request path at the given position.
+     * Ex:
+     *   position = 0, http://example.com/en/my/webservice/request
+     *   position = 2, http://example.com/my/webservice/en/request
+     *
+     * By default, it acts as a path prefix. (i.e. position 0)
+     */
+    'path' => array(
+      'settings' => array(
+        'position' => 0,
+        'en' => 'en',
+        'fr' => 'fr',
+      ),
+      'form' => 'wsdata_language_plugin_path_form',
+      'file' => 'wsdata.admin.inc',
+    ),
+    /**
+     * URI language plugin
+     *
+     * The URI plugin changes the URI to a different one based on the requesting language.
+     */
+    'uri' => array(
+      'settings' => array(
+        'en' => 'example.com',
+        'fr' => 'fr.example.com',
+      ),
+      'form' => 'wsdata_language_plugin_path_form',
+      'file' => 'wsdata.admin.inc',
+    ),
+    /**
+     * Default language plugin
+     *
+     * By default, do nothing. Assume the WsProcessor will have all the language data
+     * in a single request.
+     */
+    'default' => array(),
   );
 }
